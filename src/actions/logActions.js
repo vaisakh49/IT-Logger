aaf745ce1a23d9448f178a18420b50d1d4227a79
vaisakh <vaisakhtp133@gmail.com>
2021-08-16 01:49:39 +0530

    func searchLogs added.

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -109,6 +109,23 @@ export const updateLog = (log) => async (dispatch) => {
   }
 };
 
+export const searchLogs = (text) => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+    dispatch({
+      type: SEARCH_LOGS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
 export const setCurrent = (log) => {
   return {
     type: SET_CURRENT,
--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { connect } from 'react-redux';
+import { searchLogs } from '../../actions/logActions';
+import PropTypes from 'prop-types';
 
-export const SearchBar = () => {
+export const SearchBar = ({ searchLogs }) => {
+  const text = useRef('');
+
+  const onChange = (e) => {
+    searchLogs(text.current.value);
+  };
   return (
     <nav style={{ marginBottom: '30px' }} className='blue'>
       <div className='nav-wrapper'>
         <form>
           <div className='input-field'>
-            <input id='search' type='search' />
+            <input
+              id='search'
+              type='search'
+              placeholder='Search Logs...'
+              ref={text}
+              onChange={onChange}
+            />
             <label className='label-icon' htmlFor='search'>
               <i className='material-icons'>search</i>
             </label>
@@ -18,4 +32,8 @@ export const SearchBar = () => {
   );
 };
 
-export default SearchBar;
+SearchBar.protoTypes = {
+  searchLogs: PropTypes.func.isRequired,
+};
+
+export default connect(null, { searchLogs })(SearchBar);
